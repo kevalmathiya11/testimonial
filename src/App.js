19b6f1e9fd23e532b006841446566a0bdc3cc0a9
keval.mathiya <kevalmathiya123@gmail.com>
2024-01-29 21:43:54 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Testimonial from "./Components/Testimonial";
+import reviews from "./data";
+
+function App() {
+  return (
+    <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">Our Testimonial</h1>
+      </div>
+      <div className="bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto"></div>
+      <Testimonial reviews={reviews} />
+    </div>
+  );
+}
+
+export default App;
